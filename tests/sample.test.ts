b1ee221caf08b93d5f441747cbabb0ef730c5d39
jennymar <jenmar528@gmail.com>
2024-10-16 11:20:15 -0700

    initial linting and testing set up

---
+++ tests/sample.test.ts
@@ -0,0 +1,17 @@
+describe('dummy test', () => {
+  test('dummy test case always passes', () => {
+    expect(1 + 1).toBe(2);
+  });
+});
+
+describe('dummy test', () => {
+  test('another dummy test case always passes', () => {
+    expect(1 + 2).toBe(3);
+  });
+});
+
+describe('dummy test', () => {
+  test('dummy test will fail', () => {
+    expect(1 + 1).toBe(0);
+  });
+});
