87a8fe3240b097f482466846493e065fd6d8dd7a
jennymar <jenmar528@gmail.com>
2024-11-07 10:15:01 -0800

    linting fixes

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,33 +1,17 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: process.env.FIREBASE_API_KEY,
-//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.FIREBASE_APP_ID,
-//   measurementId: process.env.FIREBASE_MEASUREMENT_ID
-// };
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAbIs31G1I9Tp_wvAYLZRlAfGb7GPa1cuY",
-  authDomain: "hackathon-portal-2beea.firebaseapp.com",
-  projectId: "hackathon-portal-2beea",
-  storageBucket: "hackathon-portal-2beea.firebasestorage.app",
-  messagingSenderId: "772400561761",
-  appId: "1:772400561761:web:81fc3009d7b452258a3420",
-  measurementId: "G-3QSZ7Q0CPH"
+  apiKey: 'AIzaSyAbIs31G1I9Tp_wvAYLZRlAfGb7GPa1cuY',
+  authDomain: 'hackathon-portal-2beea.firebaseapp.com',
+  projectId: 'hackathon-portal-2beea',
+  storageBucket: 'hackathon-portal-2beea.firebasestorage.app',
+  messagingSenderId: '772400561761',
+  appId: '1:772400561761:web:81fc3009d7b452258a3420',
+  measurementId: 'G-3QSZ7Q0CPH',
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-export { auth,  };
+export { auth };
--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
-import { register, login } from "./authService";
+import { register, login } from './authService';
 
 register('anotheremail@gmail.com', 'password')
-.then((user) => {
-  console.log('registered user:', user)
-})
+  .then((user) => {
+    console.log('registered user:', user);
+  });
 
 
 login('randomemail@gmail.com', 'mysecretpassword')
-.then((user) => {
-  console.log('login user:', user)
-})
+  .then((user) => {
+    console.log('login user:', user);
+  });
