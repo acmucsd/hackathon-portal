014943c77586c3b819b1de649a6982b5a0d3003d
jennymar <jenmar528@gmail.com>
2024-11-07 10:08:58 -0800

    Set up firebase auth (firebase admin todo)

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log('Hello World!');
+import { register, login } from "./authService";
+
+register('anotheremail@gmail.com', 'password')
+.then((user) => {
+  console.log('registered user:', user)
+})
+
+
+login('randomemail@gmail.com', 'mysecretpassword')
+.then((user) => {
+  console.log('login user:', user)
+})
