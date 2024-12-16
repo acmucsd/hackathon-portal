9716e6c44be15f96b87627bee371cbfdcbbe466d
Alex Zhang <alexzhang1618@gmail.com>
2024-12-16 11:26:37 -0800

    Reorganize, move firebase config to .env

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAbIs31G1I9Tp_wvAYLZRlAfGb7GPa1cuY',
-  authDomain: 'hackathon-portal-2beea.firebaseapp.com',
-  projectId: 'hackathon-portal-2beea',
-  storageBucket: 'hackathon-portal-2beea.firebasestorage.app',
-  messagingSenderId: '772400561761',
-  appId: '1:772400561761:web:81fc3009d7b452258a3420',
-  measurementId: 'G-3QSZ7Q0CPH',
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
