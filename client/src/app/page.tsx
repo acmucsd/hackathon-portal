0bf08859b341d7a6b6205fff5b26bb815014401d
Sean <ussa.say@gmail.com>
2024-12-23 18:35:17 +0800

    Add common components from Figma (card, heading, button)

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,22 @@
+import Card from '@/components/Card';
 import styles from './page.module.scss';
+import Heading from '@/components/Heading';
+import Button from '@/components/Button';
 
 export default function Home() {
-  return <main className={styles.main}>hello</main>;
+  return (
+    <main className={styles.main}>
+      <Card gap={1}>
+        <Heading centered>Hello gamers</Heading>
+        <p>Once upon a time, in a galaxy far far away</p>
+        <div className={styles.buttonRow}>
+          <Button variant="tertiary">Discard Changes</Button>
+          <Button variant="secondary" href="/">
+            Save Changes
+          </Button>
+          <Button variant="primary">Next</Button>
+        </div>
+      </Card>
+    </main>
+  );
 }
