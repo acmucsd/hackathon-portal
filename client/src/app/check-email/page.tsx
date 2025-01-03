04d8c63367b06495349572108dc8b228fe5e30cd
Angela Hu <angelahu925@gmail.com>
2025-01-02 16:13:00 -0800

    More linting fixes

--- client/src/app/check-email/page.tsx
+++ client/src/app/check-email/page.tsx
@@ -9,7 +9,6 @@ interface CheckEmailProps {
 }
 
 const CheckEmail = ({ searchParams }: CheckEmailProps) => {
-
   const email = searchParams.email;
 
   return (
@@ -17,8 +16,11 @@ const CheckEmail = ({ searchParams }: CheckEmailProps) => {
       <div className={styles.check_email}>
         <Card gap={2}>
           <Heading centered>Verify your account</Heading>
-          <Typography variant="label/medium">We’ve sent an email to <b>{email}</b> to verify your email address and activate your account</Typography>
-          <Button variant="primary" href='/login'>
+          <Typography variant="label/medium">
+            We’ve sent an email to <b>{email}</b> to verify your email address and activate your
+            account
+          </Typography>
+          <Button variant="primary" href="/login">
             Return to Log In
           </Button>
         </Card>
