eed5644173508a5b4fb7b50e32fa040bfdf8a2c6
jennymar <jenmar528@gmail.com>
2025-03-03 18:39:10 -0800

    added migration file for waivers formtype in response table

---
+++ server/migrations/004-add-response-with-waivers-table.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class TestMigration1740453404021 implements MigrationInterface {
+    name = 'TestMigration1740453404021';
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query('ALTER TYPE "public"."Response_formtype_enum" RENAME TO "Response_formtype_enum_old"');
+        await queryRunner.query('CREATE TYPE "public"."Response_formtype_enum" AS ENUM(\'APPLICATION\', \'LIABILITY_WAIVER\', \'PHOTO_RELEASE\')');
+        await queryRunner.query('ALTER TABLE "Response" ALTER COLUMN "formType" TYPE "public"."Response_formtype_enum" USING "formType"::"text"::"public"."Response_formtype_enum"');
+        await queryRunner.query('DROP TYPE "public"."Response_formtype_enum_old"');
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query('CREATE TYPE "public"."Response_formtype_enum_old" AS ENUM(\'APPLICATION\')');
+        await queryRunner.query('ALTER TABLE "Response" ALTER COLUMN "formType" TYPE "public"."Response_formtype_enum_old" USING "formType"::"text"::"public"."Response_formtype_enum_old"');
+        await queryRunner.query('DROP TYPE "public"."Response_formtype_enum"');
+        await queryRunner.query('ALTER TYPE "public"."Response_formtype_enum_old" RENAME TO "Response_formtype_enum"');
+    }
+
+}
