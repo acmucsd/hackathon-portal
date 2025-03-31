418c864223dd304536e7771a9cf1d4aaec3dc7ed
Angela Hu <angelahu925@gmail.com>
2025-03-31 16:44:15 -0700

    Added waiver completion to application view

--- client/src/lib/api/AdminAPI.ts
+++ client/src/lib/api/AdminAPI.ts
@@ -7,8 +7,8 @@ import type {
   GetApplicationDecisionResponse,
   UpdateApplicationDecisionResponse,
   ResponseModel,
-  PrivateProfile,
   FullProfile,
+  GetFormsResponse,
 } from '@/lib/types/apiResponses';
 import { ApplicationDecision } from '@/lib/types/enums';
 import axios from 'axios';
@@ -64,7 +64,7 @@ export const getUsers = async (token: string): Promise<FullProfile[]> => {
  * Get user's application based on user id
  * @param token
  * @param id
- * @returns All users application
+ * @returns User's application
  */
 export const getUserWithApplication = async (token: string, id: string): Promise<ResponseModel> => {
   const requestUrl = `${config.api.baseUrl}${config.api.endpoints.admin.userApplication}/${id}`;
@@ -114,4 +114,20 @@ export const updateApplicationDecision = async (
     }
   );
   return response.data.user;
+};
+
+/**
+ * Get user's waivers based on user id
+ * @param token
+ * @param id
+ * @returns User's waivers
+ */
+export const getWaiversById = async (token: string, id: string): Promise<ResponseModel[]> => {
+  const requestUrl = `${config.api.baseUrl}${config.api.endpoints.admin.waivers}/${id}`;
+  const response = await axios.get<GetFormsResponse>(requestUrl, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data.responses;
 };
