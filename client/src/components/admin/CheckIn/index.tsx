2a6347cf4695adc3b60e18be488badad8c8d9161
Sean <ussa.say@gmail.com>
2025-04-04 13:42:07 -0700

    Generate and scan QR codes (#115)
    * QR code scanner
    
    * QR code generator
    
    * More reliable scan start
    
    * Move QR code to admin page so it doesnt show when merged
    
    * Remove console.log statement

---
+++ client/src/components/admin/CheckIn/index.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import Heading from '@/components/Heading';
+import Scanner from '../Scanner';
+import Card from '@/components/Card';
+import Button from '@/components/Button';
+import styles from './style.module.scss';
+import { useCallback } from 'react';
+
+interface CheckInProps {}
+
+const CheckIn = ({}: CheckInProps) => {
+  const handleScan = useCallback((data: string) => {
+    // TEMP
+    console.log('Scanned:', data);
+    new Audio('/assets/sounds/scan-success (TEMPORARY!!!).mp3').play();
+    if ('vibrate' in navigator) {
+      navigator.vibrate(100);
+    }
+  }, []);
+
+  return (
+    <Card gap={1.5}>
+      <Heading>QR Code Check-in</Heading>
+      <Scanner onScan={handleScan} />
+      <Button variant="secondary" href="/admin">
+        Close
+      </Button>
+    </Card>
+  );
+};
+
+export default CheckIn;
