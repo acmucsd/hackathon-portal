6748e104245436c45d815029337bdf177a3eab6c
Alex Zhang <alexzhang1618@gmail.com>
2025-04-05 00:08:53 -0700

    add dropdown fallback for checkin in case qrs brick or smth

--- client/src/app/admin/scan/page.tsx
+++ client/src/app/admin/scan/page.tsx
@@ -3,7 +3,7 @@ import { getCookie } from '@/lib/services/CookieService';
 import { CookieType } from '@/lib/types/enums';
 import styles from './page.module.scss';
 import CheckIn from '@/components/admin/CheckIn';
-import { EventAPI } from '@/lib/api';
+import { AdminAPI, EventAPI } from '@/lib/api';
 
 export default async function EventScan() {
   const accessToken = await getCookie(CookieType.ACCESS_TOKEN);
@@ -12,17 +12,18 @@ export default async function EventScan() {
     redirect('/login');
   }
 
-  let events;
+  let events, users;
   try {
     events = await EventAPI.getEvents(accessToken);
+    users = await AdminAPI.getUsers(accessToken);
   } catch (error) {
-    console.error('Failed to fetch events:', error);
+    console.error('Failed to fetch events and users:', error);
     redirect('/login');
   }
 
   return (
     <div className={styles.main}>
-      <CheckIn token={accessToken} events={events} />
+      <CheckIn token={accessToken} events={events} users={users} />
     </div>
   );
 }
