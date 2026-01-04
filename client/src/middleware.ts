0eaf69dd865f203ab514692579267b4df742aeb1
OrigamiStarz <40199724+OrigamiStarz@users.noreply.github.com>
2026-01-04 00:41:31 -0800

    Modify middleware to not redirect users to /closed

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,16 +1,21 @@
 import { NextResponse, NextRequest } from 'next/server';
-import { CookieType } from './lib/types/enums';
+// import { CookieType } from './lib/types/enums';
 
 export function middleware(request: NextRequest) {
-  const url = new URL(request.url);
-  const pathname = url.pathname;
-  const allowedPaths = ['/closed', '/_next', '/favicon', '/assets'];
 
-  if (allowedPaths.some(path => pathname.startsWith(path))) {
-    return NextResponse.next();
-  }
+  // don't block any paths
+  return NextResponse.next();
 
-  return NextResponse.redirect(new URL('/closed', request.url));
+  // uncomment the below to redirect most paths to /closed
+  // const url = new URL(request.url);
+  // const pathname = url.pathname;
+  // const allowedPaths = ['/closed', '/_next', '/favicon', '/assets'];
+
+  // if (allowedPaths.some(path => pathname.startsWith(path))) {
+  //   return NextResponse.next();
+  // }
+
+  // return NextResponse.redirect(new URL('/closed', request.url));
 }
 
 export const config = {
