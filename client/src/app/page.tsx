ce27bb348a2bdb76cd9617cde85e424ed159a337
Edward <bepbananas@gmail.com>
2026-02-22 16:25:14 -0800

    implemented occuring now component on dashboard

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,11 @@
 import { FAQ_QUESTIONS, TIMELINE } from '@/config';
 import styles from './page.module.scss';
 import Dashboard from '@/components/Dashboard';
-import { UserAPI } from '@/lib/api';
+import { EventAPI, UserAPI } from '@/lib/api';
 import { redirect } from 'next/navigation';
 import { getCookie } from '@/lib/services/CookieService';
 import { CookieType } from '@/lib/types/enums';
+import { PublicEvent } from '@/lib/types/apiResponses';
 
 export default async function Home() {
   const accessToken = await getCookie(CookieType.ACCESS_TOKEN);
@@ -18,7 +19,7 @@ export default async function Home() {
 
     return (
       <main className={styles.main}>
-        <Dashboard faq={FAQ_QUESTIONS} timeline={TIMELINE} user={fetchedUser} />
+        <Dashboard token={accessToken} faq={FAQ_QUESTIONS} timeline={TIMELINE} user={fetchedUser} />
       </main>
     );
   } catch (error) {
