a57ed14543924386b7a6b194109d63f9a6269a24
OrigamiStarz <40199724+OrigamiStarz@users.noreply.github.com>
2026-03-06 18:01:02 -0800

    Update Dashboard Theme (#185)
    * Style welcome shelf
    
    * Revise grid structure
    
    * Add banner for bottom
    
    * Add sun god image
    
    * Fix layout for submitted/not submitted
    
    * Run prettier
    
    * Modify mobile style for welcome card
    
    * Remove diamonds from footer
    
    * Small ui changes
    
    * Change order for mobile
    
    ---------
    
    Co-authored-by: Angela Hu <angelahu925@gmail.com>

--- client/src/components/Dashboard/index.tsx
+++ client/src/components/Dashboard/index.tsx
@@ -3,7 +3,9 @@
 import Image from 'next/image';
 import Card from '../Card';
 import styles from './style.module.scss';
-import Banner from '@/../public/assets/banner.png';
+import TopBanner from '@/../public/assets/banner2.png';
+import BottomBanner from '@/../public/assets/bottombanner.png';
+import SunGod from '@/../public/assets/sungod-with-book.png';
 import Typography from '../Typography';
 import Link from 'next/link';
 import FAQ, { FAQQuestion } from '../FAQAccordion';
@@ -37,147 +39,163 @@ interface DashboardProps {
 
 const Dashboard = ({ faq, timeline, user }: DashboardProps) => {
   const [showBigQr, setShowBigQr] = useState(false);
+  const confirmed = user.applicationStatus === ApplicationStatus.CONFIRMED;
 
   return (
-    <div className={styles.container}>
+    <div className={`${styles.container} ${!confirmed ? styles.notConfirmed : ''}`}>
+      {/* Welcome */}
       <Card gap={1.5} className={`${styles.card} ${styles.banner}`}>
         <Typography variant="headline/heavy/large" component="h1" className={styles.title}>
           Welcome, {user.firstName + ' ' + user.lastName}!
         </Typography>
         <Typography variant="body/medium" component="p" className={styles.subtitle}>
-          View DiamondHacks updates, resources, and check-in to events here.
+          Access the application and view DiamondHacks updates below.
         </Typography>
         <Image
-          src={Banner}
-          alt="Two diamond critters find a large jewel in a vault"
+          src={TopBanner}
+          alt="Books and potions in the shelf"
           quality={100}
           className={styles.bannerImage}
         />
       </Card>
-      {user.applicationStatus === ApplicationStatus.CONFIRMED ? (
-        <Card gap={1.5} className={`${styles.card} ${styles.status}`}>
-          <Typography variant="headline/heavy/small" component="h2">
-            QR Code Check-In
-          </Typography>
-          <QrCode data={user.id} />
-          <Typography variant="body/medium" component="p">
-            Use this QR Code to check into ACM-affiliated hackathon events, grab free food, and
-            more!
-          </Typography>
-          <Button onClick={() => setShowBigQr(true)}>Enlarge QR Code</Button>
-          <Button
-            onClick={async () => {
-              const error = await addToGoogleWallet(window.location.origin);
-              if (error) {
-                showToast('Failed to create a pass', error);
-              }
-            }}
-            variant="secondary"
-          >
-            Add to Google Wallet
-          </Button>
-        </Card>
-      ) : (
-        <Card gap={1.5} className={`${styles.card} ${styles.status}`}>
-          <Typography variant="headline/heavy/small" component="h2">
-            Application Status
-          </Typography>
-          <DashboardStatus status={user.applicationStatus as Status} timeline={timeline} />
-        </Card>
+      {confirmed && (
+        <>
+          {/* QR Code */}
+          <Card gap={1.5} className={`${styles.card} ${styles.status}`}>
+            <Typography variant="headline/heavy/small" component="h2">
+              QR Code Check-In
+            </Typography>
+            <QrCode data={user.id} />
+            <Typography variant="body/medium" component="p">
+              Use this QR Code to check into ACM-affiliated hackathon events, grab free food, and
+              more!
+            </Typography>
+            <Button onClick={() => setShowBigQr(true)}>Enlarge QR Code</Button>
+            <Button
+              onClick={async () => {
+                const error = await addToGoogleWallet(window.location.origin);
+                if (error) {
+                  showToast('Failed to create a pass', error);
+                }
+              }}
+              variant="secondary"
+            >
+              Add to Google Wallet
+            </Button>
+          </Card>
+          {/* Onboarding */}
+          <Card gap={1.5} className={`${styles.card} ${styles.onboarding}`}>
+            <Typography variant="headline/heavy/small" component="h2">
+              Onboarding Checklist
+            </Typography>
+            <ul className={styles.onboardingChecklist}>
+              <li>
+                <Typography variant="body/large">
+                  Fill out the{' '}
+                  <Link href="/photoRelease" className={styles.link}>
+                    photo release waiver
+                  </Link>
+                </Typography>
+              </li>
+              <li>
+                <Typography variant="body/large">
+                  Fill out the{' '}
+                  <Link href="/liability" className={styles.link}>
+                    liability waiver
+                  </Link>
+                </Typography>
+              </li>
+              <li>
+                <Typography variant="body/large">
+                  Join the{' '}
+                  <Link
+                    href="http://acmurl.com/diamondhacks25-discord"
+                    className={styles.link}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    ACM DiamondHacks Discord
+                  </Link>{' '}
+                  for real-time, up-to-date announcements
+                </Typography>
+              </li>
+              <li>
+                <Typography variant="body/large">
+                  Check out the{' '}
+                  <Link
+                    href="http://acmurl.com/diamondhacks25-guide"
+                    className={styles.link}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    ACM DiamondHacks Hacker Guide
+                  </Link>
+                </Typography>
+              </li>
+              <li>
+                <Typography variant="body/large">
+                  Check out the{' '}
+                  <Link href="/schedule" className={styles.link}>
+                    schedule
+                  </Link>{' '}
+                  of events and workshops
+                </Typography>
+              </li>
+            </ul>
+          </Card>
+        </>
       )}
-      {user.applicationStatus === ApplicationStatus.CONFIRMED ? (
-        <Card gap={1.5} className={`${styles.card} ${styles.timeline}`}>
-          <Typography variant="headline/heavy/small" component="h2">
-            Onboarding Checklist
-          </Typography>
-          <ul className={styles.onboardingChecklist}>
-            <li>
-              <Typography variant="body/large">
-                Fill out the{' '}
-                <Link href="/photoRelease" className={styles.link}>
-                  photo release waiver
-                </Link>
-              </Typography>
-            </li>
-            <li>
-              <Typography variant="body/large">
-                Fill out the{' '}
-                <Link href="/liability" className={styles.link}>
-                  liability waiver
-                </Link>
-              </Typography>
-            </li>
-            <li>
-              <Typography variant="body/large">
-                Join the{' '}
-                <Link
-                  href="http://acmurl.com/diamondhacks25-discord"
-                  className={styles.link}
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  ACM DiamondHacks Discord
-                </Link>{' '}
-                for real-time, up-to-date announcements
-              </Typography>
-            </li>
-            <li>
-              <Typography variant="body/large">
-                Check out the{' '}
-                <Link
-                  href="http://acmurl.com/diamondhacks25-guide"
-                  className={styles.link}
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  ACM DiamondHacks Hacker Guide
-                </Link>
-              </Typography>
-            </li>
-            <li>
-              <Typography variant="body/large">
-                Check out the{' '}
-                <Link href="/schedule" className={styles.link}>
-                  schedule
-                </Link>{' '}
-                of events and workshops
-              </Typography>
-            </li>
-          </ul>
-        </Card>
-      ) : (
-        <Card gap={1.5} className={`${styles.card} ${styles.timeline}`}>
-          <Typography variant="headline/heavy/small" component="h2">
-            Timeline
-          </Typography>
-          <div className={styles.timelineItemWrapper}>
-            <TimelineItem date={timeline.application} first>
-              Application Deadline
-            </TimelineItem>
-            <TimelineItem date={timeline.decisions}>Decisions Released</TimelineItem>
-            <TimelineItem date={timeline.acceptance}>RSVP Deadline</TimelineItem>
-            <TimelineItem date={timeline.waitlist}>Rolling Waitlist RSVP</TimelineItem>
-            <TimelineItem date={timeline.hackathon}>Hackathon Day!</TimelineItem>
-          </div>
-        </Card>
+      {!confirmed && (
+        <>
+          <Card gap={1.5} className={`${styles.card} ${styles.status}`}>
+            <Typography variant="headline/heavy/small" component="h2">
+              Application Status
+            </Typography>
+            <DashboardStatus status={user.applicationStatus as Status} timeline={timeline} />
+          </Card>
+          <Card gap={1.5} className={`${styles.card} ${styles.timeline}`}>
+            <Typography variant="headline/heavy/small" component="h2">
+              Timeline
+            </Typography>
+            <div className={styles.timelineItemWrapper}>
+              <TimelineItem date={timeline.application} first>
+                Application Deadline
+              </TimelineItem>
+              <TimelineItem date={timeline.decisions}>Decisions Released</TimelineItem>
+              <TimelineItem date={timeline.acceptance}>RSVP Deadline</TimelineItem>
+              <TimelineItem date={timeline.waitlist}>Rolling Waitlist RSVP</TimelineItem>
+              <TimelineItem date={timeline.hackathon}>Hackathon Day!</TimelineItem>
+            </div>
+          </Card>
+        </>
       )}
-      {user.applicationStatus === ApplicationStatus.CONFIRMED ? null : (
-        <Card gap={1.5} className={`${styles.card} ${styles.faq}`}>
-          <Typography variant="headline/heavy/small" component="h2">
-            Frequently Asked Questions
-          </Typography>
-          <div>
-            <FAQ data={faq} />
-          </div>
-          <Typography variant="body/large" component="p">
-            Still have questions? Email{' '}
-            <Link href="mailto:hackathon@acmucsd.org" className="link">
-              hackathon@acmucsd.org
-            </Link>{' '}
-            to reach DiamondHacks’s organizers!
-          </Typography>
-        </Card>
-      )}
+      <Card gap={1.5} className={`${styles.card} ${styles.faq}`}>
+        <Typography variant="headline/heavy/small" component="h2">
+          Frequently Asked Questions
+        </Typography>
+        <div>
+          <FAQ data={faq} />
+        </div>
+        <Typography variant="body/large" component="p" className={styles.stillHaveQuestions}>
+          Still have questions? Email{' '}
+          <Link href="mailto:hackathon@acmucsd.org" className="link">
+            hackathon@acmucsd.org
+          </Link>{' '}
+          to reach DiamondHacks’s organizers!
+        </Typography>
+        <Image
+          src={SunGod}
+          alt="Sun God holding a book"
+          quality={100}
+          className={styles.sunGodImage}
+        />
+      </Card>
+      <Image
+        src={BottomBanner}
+        alt="Banners of the four houses: Raccoon, Sun God, Geisel, and Triton"
+        quality={100}
+        className={styles.banner2}
+      />
       <Modal
         title={`${user.firstName} ${user.lastName}'s QR Code`}
         open={showBigQr}
