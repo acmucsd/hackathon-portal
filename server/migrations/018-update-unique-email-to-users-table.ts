5584bab5a580d3e304c20b3e5e34592cdf40e8a0
Nandita Raj Kumar <nanditarajkumar@gmail.com>
2026-03-25 21:24:50 -0700

    update the email field to be unique for the users table

---
+++ server/migrations/018-update-unique-email-to-users-table.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class UpdateUniqueEmailToUsersTable1774498320669 implements MigrationInterface {
+    name = 'UpdateUniqueEmailToUsersTable1774498320669'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "User" ADD CONSTRAINT "UQ_4a257d2c9837248d70640b3e36e" UNIQUE ("email")`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "User" DROP CONSTRAINT "UQ_4a257d2c9837248d70640b3e36e"`);
+    }
+
+}
