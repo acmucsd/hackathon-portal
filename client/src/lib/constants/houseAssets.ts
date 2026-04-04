31f57d72aa4c908d6b97a6e2fc16012c67e92d24
Toby Chin <92127289+spagechi@users.noreply.github.com>
2026-04-03 20:19:43 -0700

    add house mascots to dashboard

--- client/src/lib/constants/houseAssets.ts
+++ client/src/lib/constants/houseAssets.ts
@@ -1,4 +1,4 @@
-import { confetti, confettiMobile, badges, books } from '@/../public/assets/houses';
+import { confetti, confettiMobile, badges, books, mascotDashboards, mascotDashboardsMobile } from '@/../public/assets/houses';
 import { House } from '../types/enums';
 import { StaticImageData } from 'next/image';
 
@@ -7,6 +7,8 @@ export interface HouseAssets {
   confettiMobile: StaticImageData | string;
   badge: StaticImageData | string;
   books: StaticImageData | string;
+  mascotDashboard: StaticImageData | string;
+  mascotDashboardMobile: StaticImageData | string;
 }
 
 export const houseAssets: Record<House, HouseAssets> = {
@@ -15,29 +17,39 @@ export const houseAssets: Record<House, HouseAssets> = {
     confettiMobile: confettiMobile.green,
     badge: badges.raccoon,
     books: books.green,
+    mascotDashboard: mascotDashboards.raccoon,
+    mascotDashboardMobile: mascotDashboardsMobile.raccoon,
   },
   [House.SUN_GOD]: {
     confetti: confetti.red,
     confettiMobile: confettiMobile.red,
     badge: badges.sunGod,
     books: books.red,
+    mascotDashboard: mascotDashboards.sunGod,
+    mascotDashboardMobile: mascotDashboardsMobile.sunGod,
   },
   [House.GEISEL]: {
     confetti: confetti.blue,
     confettiMobile: confettiMobile.blue,
     badge: badges.geisel,
     books: books.blue,
+    mascotDashboard: mascotDashboards.geisel,
+    mascotDashboardMobile: mascotDashboardsMobile.geisel,
   },
   [House.TRITON]: {
     confetti: confetti.yellow,
     confettiMobile: confettiMobile.yellow,
     badge: badges.kingTriton,
     books: books.yellow,
+    mascotDashboard: mascotDashboards.kingTriton,
+    mascotDashboardMobile: mascotDashboardsMobile.kingTriton,
   },
   [House.UNASSIGNED]: {
     confetti: '',
     confettiMobile: '',
     badge: '',
     books: '',
+    mascotDashboard: '',
+    mascotDashboardMobile: '',
   },
 } as const;
